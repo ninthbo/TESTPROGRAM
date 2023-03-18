98c4d20f6b1078e7591d0c09303c39b3f5202a62
ninthbo <109381700+ninthbo@users.noreply.github.com>
2023-03-18 14:15:03 +0000

    fixed types

--- gradlew.bat
+++ gradlew.bat
@@ -11,3 +11,11 @@
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
+
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
