4a9cdfe2c9d94376f693555cd2279d94318dfcce
ninthbo <109381700+ninthbo@users.noreply.github.com>
2023-03-19 09:48:31 +0000

    Fix README typo

--- gradlew.bat
+++ gradlew.bat
@@ -19,3 +19,16 @@
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
