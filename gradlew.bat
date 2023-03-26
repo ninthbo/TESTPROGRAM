fc00de140f41ce6ce6958cb9a5ec835dc757d04e
ninthbo <109381700+ninthbo@users.noreply.github.com>
2023-03-26 13:25:51 +0000

    irda: sir_dev: Fix copy/paste typo

--- gradlew.bat
+++ gradlew.bat
@@ -48,3 +48,11 @@ echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PA
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto execute
