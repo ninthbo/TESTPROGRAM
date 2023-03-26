0fa7633e27d442995967af8d9f4277d38888dd36
ninthbo <109381700+ninthbo@users.noreply.github.com>
2023-03-26 22:38:42 +0000

    Updated scripts.

--- gradlew.bat
+++ gradlew.bat
@@ -62,3 +62,11 @@ echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
+
+goto fail
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
