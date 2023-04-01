9c682032bf3c63947ee7fa1cdda4bb5e390e4a3c
ninthbo <109381700+ninthbo@users.noreply.github.com>
2023-04-01 22:32:00 +0000

    Remove unused test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,3 +3,13 @@ const val PRICE = 100
 const val DISCOUNT = 100
 const val DISCOUNT5 = 0.05
 const val MELOMAN = 0.01
+
+fun main(){
+    val minBuy = 1_000
+    val maxBuy = 10_000
+    print("Введите количество песен для покупки: ")
+    val products = readLine()!!.toUInt()
+    println("Покупали ли вы музыку в прошлом месяце? (y/n)")
+    val isMeloman = (readLine()!!.toString()) == "y"
+
+    var totalPrice: Double = PRICE * products.toDouble()
